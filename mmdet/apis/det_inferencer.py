0c797e95ecf51037f99eff8e21310f6429c6cf69
Khanh Nguyen <khanhhdz111@gmail.com>
2025-01-05 10:43:55 +0700

    skip visualization

--- mmdet/apis/det_inferencer.py
+++ mmdet/apis/det_inferencer.py
@@ -401,17 +401,7 @@ class DetInferencer(BaseInferencer):
         for ori_imgs, data in (track(inputs, description='Inference')
                                if self.show_progress else inputs):
             preds = self.forward(data, **forward_kwargs)
-            visualization = self.visualize(
-                ori_imgs,
-                preds,
-                return_vis=return_vis,
-                show=show,
-                wait_time=wait_time,
-                draw_pred=draw_pred,
-                pred_score_thr=pred_score_thr,
-                no_save_vis=no_save_vis,
-                img_out_dir=out_dir,
-                **visualize_kwargs)
+            visualization = None
             results = self.postprocess(
                 preds,
                 visualization,
